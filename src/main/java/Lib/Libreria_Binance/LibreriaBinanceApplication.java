b803790b1f03c1aec186e952a7ac9d2323b57b6c
ManuelLazaro <gorselazarorosas@gmail.com>
2025-06-12 20:18:03 -0500

    init

---
+++ src/main/java/Lib/Libreria_Binance/LibreriaBinanceApplication.java
@@ -0,0 +1,13 @@
+package Lib.Libreria_Binance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaBinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibreriaBinanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Lib/Libreria_Binance/LibreriaBinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package Lib.Libreria_Binance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaBinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
